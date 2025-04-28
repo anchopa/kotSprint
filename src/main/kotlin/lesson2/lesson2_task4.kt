9e70b348a3626946dd33097131f1687ee11cc0b2
chopa <danil.chebotaev.00@mail.ru>
2025-04-28 22:07:02 +0300

    KS-2-4 smth gagarin

---
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson2
+
+fun main() {
+
+    val length = 40_868_600_000L
+    val age = 27
+    val partOfDay = 0.075f
+    val seconds = 6480
+    val partOfYear = 2.0547945205479453E-4
+    val apogee = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
