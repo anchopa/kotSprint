dfd55b42c63be8e5000de76b359926515044e274
chopa <danil.chebotaev.00@mail.ru>
2025-04-29 00:15:22 +0300

    KS-3-1 sr

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson3
+
+fun main() {
+
+    val vasuaGrade = 3.00
+    val anyaGrade = 4.00
+    val sashaGrade = 3.00
+    val mashaGrade = 5.00
+
+    val sum = vasuaGrade+anyaGrade+sashaGrade+mashaGrade
+    val srGrade = sum / 4
+
+    println(srGrade)
+}
