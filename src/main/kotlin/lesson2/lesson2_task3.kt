6084742517d6d3a2ad1bd6ce01d9e7e71aaa2e00
chopa <danil.chebotaev.00@mail.ru>
2025-04-28 21:53:56 +0300

    KS-2-3 gagarin

---
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson2
+
+fun main() {
+
+    val yearOfGagarin = 1961
+    var hour = 9
+    var minute = 7
+
+//    println(hour)
+//    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
+
+
+}
