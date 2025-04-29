396c5b977c5becede3430675ee4cd4591120de69
chopa <danil.chebotaev.00@mail.ru>
2025-04-29 16:06:25 +0300

    KS-2-3 string>int

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -3,14 +3,14 @@ package org.example.lesson2
 fun main() {
 
     val yearOfGagarin = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
 //    println(hour)
 //    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("$hour:$minute")
 
