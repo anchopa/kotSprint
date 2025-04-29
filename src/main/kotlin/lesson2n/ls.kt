aeb2a33ea6afc7e73470d6fbab44dbb3cc5146be
chopa <danil.chebotaev.00@mail.ru>
2025-04-29 17:58:31 +0300

    KS-2-5 const in

--- src/main/kotlin/lesson2n/ls.kt
+++ src/main/kotlin/lesson2n/ls.kt
@@ -1,17 +1,18 @@
 package org.example.lesson2n
 
+    const val SECONDS_IN_HOUR = 3600
+    const val SECONDS_IN_MINUTE = 60
+    const val SECONDS_TOTAL = 6480
+
+
 fun main() {
 
-    val seconds = 6480
 
-    val hour = seconds / 3600
+    val seconds = SECONDS_TOTAL
+    val hour = seconds / SECONDS_IN_HOUR
+    val remainingSeconds = seconds % SECONDS_IN_HOUR
+    val minutes = remainingSeconds / SECONDS_IN_MINUTE
+    val secAfterMin = remainingSeconds % SECONDS_IN_MINUTE
 
-    val ostatokSeconds = seconds % 3600
-
-    val minutes = ostatokSeconds / 60
-
-    val secAfterMin = ostatokSeconds % 60
-
-
-    println("${"%02d:%02d:%02d".format(hour, minutes, seconds)}")
+    println("%02d:%02d:%02d".format(hour, minutes, secAfterMin))
 }
