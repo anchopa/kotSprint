fe41318b3e2f18006d347a632fca2f5cd0625aec
sssamayovna <bbordderline@gmail.com>
2025-05-02 20:33:49 +0400

    KS-2-3 add buffPercent

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -1,14 +1,19 @@
 package org.example.lesson3
 
+const val percent = 100.0
+
 fun main() {
+
 
     val crystalsWithoutBuff = 7
     val ironWithoutBuff = 11
-    val buff = 0.2
+    val buffPercent = 20.0
 
-    val crystalBonus = (crystalsWithoutBuff * buff).toInt()
-    val ironBonus = (ironWithoutBuff * buff).toInt()
+
+    val crystalBonus = (crystalsWithoutBuff * (buffPercent / percent)).toInt()
+    val ironBonus = (ironWithoutBuff * (buffPercent / percent)).toInt()
 
     println("Кристаллическая руда: $crystalBonus")
     println("Железная руда: $ironBonus")
+
 }
